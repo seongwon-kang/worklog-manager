5a7e5d04d94826f366a3e9c8cf4c07bb7427af54
seongwon <abc7988se@gmail.com>
2019-10-18 17:15:21 +0900

    First Run

---
+++ src/main/java/io/seongwon/worklogmanager/DefaultDatabaseConfig.java
@@ -0,0 +1,8 @@
+package io.seongwon.worklogmanager;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DefaultDatabaseConfig {
+
+}
--- src/main/java/io/seongwon/worklogmanager/WorklogManagerApplication.java
+++ src/main/java/io/seongwon/worklogmanager/WorklogManagerApplication.java
@@ -1,9 +1,14 @@
 package io.seongwon.worklogmanager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
+@ComponentScan(basePackages = "io.seongwon")
 public class WorklogManagerApplication {
 
 	public static void main(String[] args) {
